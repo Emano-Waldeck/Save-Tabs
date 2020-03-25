65e6c002ec4cb6d8f050847b203a9a54b2bc5990
Emano Waldeck <emano.waldeck@gmail.com>
2020-03-25 07:31:55 +0100

    favicon on the discarded state

--- firefox/data/discard/index.js
+++ firefox/data/discard/index.js
@@ -2,13 +2,20 @@
 
 const args = new URLSearchParams(location.search);
 
-document.title = args.get('title') || args.get('href');
 
-if (document.hidden) {
-  document.addEventListener('visibilitychange', () => {
-    location.replace(args.get('href'));
-  });
+if (args.has('href')) {
+  const href = args.get('href');
+  document.title = args.get('title') || href;
+  if (document.hidden) {
+    document.addEventListener('visibilitychange', () => {
+      location.replace(href);
+    });
+    if (/Firefox/.test(navigator.userAgent) === false) {
+      document.querySelector('link[rel="icon"]').href = 'chrome://favicon/' + href;
+    }
+  }
+  else {
+    location.replace(href);
+  }
 }
-else {
-  location.replace(args.get('href'));
-}
+
